f3e0f5358498c681a8591d41a94dc91f384ff51f
GabriielSantos <gabrielcsantos.dev@gmail.com>
2023-05-16 15:24:45 -0300

    feat: Implement Components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,60 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom/cjs/react-router-dom.min';
+import Album from './pages/Album';
+import Favorites from './pages/Favorites';
+import Login from './pages/Login';
+import NotFound from './pages/NotFound';
+import Profile from './pages/Profile';
+import ProfileEdit from './pages/ProfileEdit';
+import Search from './pages/Search';
 
 class App extends React.Component {
   render() {
-    return (<p>TrybeTunes!</p>);
+    return (
+      <div>
+        <Switch>
+
+          <Route
+            path="/"
+            component={ Login }
+            exact
+          />
+
+          <Route
+            path="/search"
+            component={ Search }
+            exact
+          />
+
+          <Route
+            path="/album/:id"
+            component={ Album }
+            exact
+          />
+
+          <Route
+            path="/favorites"
+            component={ Favorites }
+            exact
+          />
+
+          <Route
+            path="/profile"
+            component={ Profile }
+            exact
+          />
+
+          <Route
+            path="/profile/edit"
+            component={ ProfileEdit }
+            exact
+          />
+
+          <Route path="*" component={ NotFound } />
+
+        </Switch>
+      </div>
+    );
   }
 }
 
---
+++ src/components/MainLayout.jsx
@@ -0,0 +1,26 @@
+// import PropTypes from 'prop-types';
+// import React from 'react';
+// import '../css/MainLayout.css';
+// import Header from './Header';
+// import Sidebar from './Sidebar';
+
+// class MainLayout extends React.Component {
+//   render() {
+//     console.log(this.props.children);
+//     return (
+//       <>
+//         <Header />
+//         <main>
+//           <Sidebar />
+//           <div className="wrapper" />
+//           { this.props.children }
+//         </main>
+//       </>
+//     );
+//   }
+// }
+
+// MainLayout.propTypes = {
+//   children: PropTypes.node.isRequired,
+// };
+// export default MainLayout;
