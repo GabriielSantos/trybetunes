e6562d4eb02e6f4316b58a310f405e5c38cb628a
GabriielSantos <gabrielcsantos.dev@gmail.com>
2023-05-16 18:12:16 -0300

    feat: Implement Component Header

---
+++ src/components/Header.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import { getUser } from '../services/userAPI';
+import Loading from './Loading';
+
+class Header extends React.Component {
+  state = {
+    loading: true,
+    userName: '',
+  };
+
+  async componentDidMount() {
+    try {
+      const user = await getUser();
+      this.setState({
+        loading: false,
+        userName: user.name,
+      });
+    } catch (error) {
+      console.error('Erro ao encontrar o nome de usuário', erro);
+      this.setState({
+        loading: false,
+      });
+    }
+  }
+
+  render() {
+    const { loading, userName } = this.state;
+    if (loading) {
+      return <Loading />;
+    }
+    return (
+      <header data-testid="header-component">
+        <nav>
+          <NavLink to="/search" activeClassName="active" data-testid="link-to-search">
+            Pesquisa
+          </NavLink>
+          <NavLink
+            to="/favorites"
+            activeClassName="active"
+            data-testid="link-to-favorites"
+          >
+            Favoritas
+          </NavLink>
+          <NavLink to="/profile" activeClassName="active" data-testid="link-to-profile">
+            Profile
+          </NavLink>
+        </nav>
+        <p data-testid="header-user-name">{userName}</p>
+      </header>
+    );
+  }
+}
+
+export default Header;
